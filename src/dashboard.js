70604858f856c1f9820a46fc63e7c1cb8cf83e7b
carolineablynch <carolineablynch@gmail.com>
2021-03-30 21:11:34 -0400

    starting employeeTable.js file

---
+++ src/dashboard.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import EmployeeTable from "./EmployeeTable";
+import Sort from "./Sort";
+import Filter from "./Filter";
+
+export default function Dashboard({ employees }) {
+    const [selectedEmployees, setSelectedEmployees] = useState(employees);
+  
+    return (
+      <>
+        <h1>Employees</h1>
+        <Sort employees={selectedEmployees} onChange={setSelectedEmployees} />
+        <Filter employees={employees} onChange={setSelectedEmployees} />
+        <EmployeeTable employees={selectedEmployees} />
+      </>
+    );
+  }
