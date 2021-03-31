1716be7de935aa8eeb38bcb9c72cd38403a55658
carolineablynch <carolineablynch@gmail.com>
2021-03-30 21:09:37 -0400

    finish app function

--- src/app.js
+++ src/app.js
@@ -21,3 +21,36 @@ async function fetchEmployees({ success, fail }) {
     const [employees, setSemployees] = useState(null);
     const [fetching, setFetching] = useState(false);
     const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if(!employees) {
+          setFetching(true);
+          fetchEmployees({
+            success: data => {
+              setSemployees(data);
+              setFetching(false);
+              setError(null);
+            },
+            fail: () => {
+              setError({ message: "Unable to fetch employees" });
+              setFetching(false);
+            }
+          });
+        }
+      }, [employees]);
+    return (
+        <div className="App">
+          <div style={{
+            background: "linear-gradient(180deg, rgba(255,198,181,1) 30%, rgba(110,202,175,1) 100%, rgba(159,238,126,0.6223623238357843) 100%)",
+            height: "100px"
+          }}>
+            <h1>Employee Dashboard</h1>
+          </div>
+          {error && <Error error={error} />}
+          {fetching && "fetching..."}
+          {employees && <Dashboard employees={employees.results} />}
+        </div>
+      );
+    }
+    
+    export default App;
